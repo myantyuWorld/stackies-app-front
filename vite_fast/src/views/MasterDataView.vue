87099f742b4d419c67e097c9dd54542be7963cb2
myantyuWorld <nepinepi984@gmail.com>
2023-04-08 18:42:23 +0900

    技術マスタビュー、piniaに処理を移設

--- vite_fast/src/views/MasterDataView.vue
+++ vite_fast/src/views/MasterDataView.vue
@@ -7,71 +7,79 @@ import Loading from '../components/Loading.vue'
 import { useStore } from '@/stores/store' 
 import { toRefs } from 'vue';
 import { Authenticator, useAuthenticator } from '@aws-amplify/ui-vue';
+import {useStacikesStore} from '@/stores/store'
 
+const stakiesStore = useStacikesStore();
 // https://ui.docs.amplify.aws/vue/connected-components/authenticator/headless
 const { route, user, signOut } = toRefs(useAuthenticator());
-console.log(user.value.username) // "hogehoge"
-
-const languageList = ref([])
-const toolList = ref([])
-const infraList = ref([])
-
 const isShowLoading = ref(false)
+
+const init = () => {
+  stakiesStore.fetchTechnologies()
+  stakiesStore.fetchExperienceTechnologies(user.value.username)
+}
+init()
+
+// const languageList = ref([])
+// const toolList = ref([])
+// const infraList = ref([])
+
+
 /**
  * 技術マスタから技術レコードを取得する
  */
-const fetchTechnologies = () => {
-  isShowLoading.value = true
-  const promise = axios.get(`${import.meta.env.VITE_APP_API_URL}technologies`)
-  promise
-    .then((response) => {
-      console.log(response.data)
-      let list: { category: string; name: string; }[] = []
-      let list2: { category: string; name: string; }[] = []
-      let list3: { category: string; name: string; }[] = []
-      response.data.filter((value: String) => {
-        return value["category"]["S"] === "1"
-      }).forEach((item: any) => {
-        list.push({
-          "category": item["category"]["S"],
-          "name": item["name"]["S"]
-        })
-      });
-      response.data.filter((value: String) => {
-        return value["category"]["S"] === "2"
-      }).forEach((item: any) => {
-        list2.push({
-          "category": item["category"]["S"],
-          "name": item["name"]["S"]
-        })
-      });
-      response.data.filter((value: String) => {
-        return value["category"]["S"] === "3"
-      }).forEach((item: any) => {
-        list3.push({
-          "category": item["category"]["S"],
-          "name": item["name"]["S"]
-        })
-      });
-      // データ加工
-      return Promise.resolve({
-        list,
-        list2,
-        list3
-      })
-    })
-    .then((data) => {
-      languageList.value = data.list
-      infraList.value = data.list2
-      toolList.value = data.list3
-      isShowLoading.value = false
-    })
-    .catch((e) => {
-      alert(e.message)
-      isShowLoading.value = false
-    })
-}
-fetchTechnologies()
+// const fetchTechnologies = () => {
+//   isShowLoading.value = true
+//   const promise = axios.get(`${import.meta.env.VITE_APP_API_URL}technologies`)
+//   promise
+//     .then((response) => {
+//       console.log(response.data)
+//       let list: { category: string; name: string; }[] = []
+//       let list2: { category: string; name: string; }[] = []
+//       let list3: { category: string; name: string; }[] = []
+//       response.data.filter((value: String) => {
+//         return value["category"]["S"] === "1"
+//       }).forEach((item: any) => {
+//         list.push({
+//           "category": item["category"]["S"],
+//           "name": item["name"]["S"]
+//         })
+//       });
+//       response.data.filter((value: String) => {
+//         return value["category"]["S"] === "2"
+//       }).forEach((item: any) => {
+//         list2.push({
+//           "category": item["category"]["S"],
+//           "name": item["name"]["S"]
+//         })
+//       });
+//       response.data.filter((value: String) => {
+//         return value["category"]["S"] === "3"
+//       }).forEach((item: any) => {
+//         list3.push({
+//           "category": item["category"]["S"],
+//           "name": item["name"]["S"]
+//         })
+//       });
+//       // データ加工
+//       return Promise.resolve({
+//         list,
+//         list2,
+//         list3
+//       })
+//     })
+//     .then((data) => {
+//       languageList.value = data.list
+//       infraList.value = data.list2
+//       toolList.value = data.list3
+//       isShowLoading.value = false
+//     })
+//     .catch((e) => {
+//       alert(e.message)
+//       isShowLoading.value = false
+//     })
+// }
+// fetchTechnologies()
 
 /**
  * 経験のある技術を選択した結果を、APIリクエストする
@@ -81,9 +89,9 @@ const click_regist = () => {
   console.log("click_regist")
   console.log("=====")
 
-  var selected_list = languageList.value.filter(x => x.checked)
-  selected_list.push(toolList.value.filter(x => x.checked))
-  selected_list.push(infraList.value.filter(x => x.checked))
+  var selected_list = stakiesStore.languageList.filter(x => x.checked)
+  selected_list.push(stakiesStore.toolList.filter(x => x.checked))
+  selected_list.push(stakiesStore.infraList.filter(x => x.checked))
   console.log(selected_list.flat(2))
 
   var data: { name: any; category: any; level: string; }[] = []
@@ -136,13 +144,13 @@ const click_regist = () => {
                   あなたがこれまで経験してきた技術を選択して、登録しましょう。
                 </p>
               </div>
-              <MasterData :list=languageList name="言語" itemBadgeColor="blue"
+              <MasterData :list=stakiesStore.languageList name="言語" itemBadgeColor="blue"
                 :class="[isShowLoading ? 'opacity-40' : '']" />
 
-              <MasterData :list=toolList name="DB・ツール" itemBadgeColor="rose"
+              <MasterData :list=stakiesStore.toolList name="DB・ツール" itemBadgeColor="rose"
                 :class="[isShowLoading ? 'opacity-40' : '']" />
 
-              <MasterData :list=infraList name="動作環境" itemBadgeColor="lime"
+              <MasterData :list=stakiesStore.infraList name="動作環境" itemBadgeColor="lime"
                 :class="[isShowLoading ? 'opacity-40' : '']" />
 
             </div>
