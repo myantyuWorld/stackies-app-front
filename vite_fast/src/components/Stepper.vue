59c557d7545d2702d70d4a7413411ce114c6c66b
myantyuWorld <nepinepi984@gmail.com>
2023-04-15 13:31:20 +0900

    Modify Stepper

--- vite_fast/src/components/Stepper.vue
+++ vite_fast/src/components/Stepper.vue
@@ -1,60 +1,44 @@
 <script setup lang="ts">
 import Rating from '../components/Rating.vue'
-// defineProps<{
-//   msg: string
-// }>()
 </script>
 <template>
-  <ol class="relative m-4 text-gray-500 border-l border-gray-200 dark:border-gray-700 dark:text-gray-400">
-    <li class="mb-10 ml-6">
+  <ol class="items-center w-full space-y-4 sm:flex sm:space-x-8 sm:space-y-0">
+    <li class="w-1/3 flex items-center text-blue-600 dark:text-blue-500 space-x-2.5">
       <span
-        class="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
-        <svg aria-hidden="true" class="w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd"
-            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-            clip-rule="evenodd"></path>
-        </svg>
+        class="flex items-center justify-center w-8 h-8 border border-blue-600 rounded-full shrink-0 dark:border-blue-500">
+        1
       </span>
-      <h3 class="font-medium leading-tight">
-        <RouterLink to="/master">経験技術の登録</RouterLink>
-      </h3>
-      <p class="text-sm">経験のある言語、ツールを選択し、登録ボタンをクリック</p>
+      <span>
+        <RouterLink to="/master">経験技術の登録
+          <p class="text-sm">経験のある言語、ツールを選択し、登録ボタンをクリック</p>
+        </RouterLink>
+      </span>
     </li>
-
-    <li class="mb-10 ml-6">
+    <li class="w-1/3 flex items-center text-gray-500 dark:text-gray-400 space-x-2.5">
       <span
-        class="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-gray-700">
-        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg">
-          <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
-          <path fill-rule="evenodd"
-            d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
-            clip-rule="evenodd"></path>
-        </svg>
+        class="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
+        2
       </span>
-      <h3 class="font-medium leading-tight">
-        <RouterLink to="/register">基本能力の登録</RouterLink>
-      </h3>
-      <p class="text-sm">あなたの経歴概要と、「経験技術の登録」で選択した技術のレベルを選択し、登録ボタンをクリック</p>
+      <span>
+        <RouterLink to="/register">基本能力の登録
+          <p class="text-sm">あなたの経歴概要と、「経験技術の登録」で選択した技術のレベルを選択し、登録ボタンをクリック</p>
+        </RouterLink>
+      </span>
     </li>
-    <li class="ml-6">
+    <li class="w-1/3 flex items-center text-gray-500 dark:text-gray-400 space-x-2.5">
       <span
-        class="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-gray-700">
-        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg">
-          <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
-          <path fill-rule="evenodd"
-            d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm9.707 5.707a1 1 0 00-1.414-1.414L9 12.586l-1.293-1.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-            clip-rule="evenodd"></path>
-        </svg>
+        class="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
+        3
       </span>
-      <h3 class="font-medium leading-tight">
-        <RouterLink to="/projects">案件対応履歴の表示</RouterLink>
-      </h3>
-      <p class="text-sm">新規で案件対応を追加したい場合は、「業務履歴」横の追加ボタンから登録</p>
+      <span>
+        <RouterLink to="/projects">案件対応履歴の表示
+          <p class="text-sm">新規で案件対応を追加したい場合は、「業務履歴」横の追加ボタンから登録</p>
+        </RouterLink>
+      </span>
     </li>
   </ol>
+
+  
   <div id="" class=" bg-gray-100  py-8 rounded-xl">
     <span>技術レベルの目安</span>
     <div class="flex flex-wrap justify-items-center">
